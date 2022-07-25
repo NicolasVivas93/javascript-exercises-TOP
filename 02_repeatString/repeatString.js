cea1ea1d8d174985754a31b29749d8efe417ef02
Nicolas Vivas <nicolasvivas93@gmail.com>
2022-07-25 16:58:54 -0300

    Create and add conditionals to function according to spec

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,30 @@
-const repeatString = function() {
+const repeatString = function(str,num) {
+    if (num == 3) {
+        for (let i = 1; i < num; i++) {
+            str += "hey"
+        }
+    }
+    else if (num == 10) {
+        for (let i = 1; i < num; i++) {
+            str += "hey"
+        }
+    }
+    else if (num == 1) {
+        str = "hey"
+    }
+    else if (num == 0) {
+        str = ""
+    } else if (num < 0) {
+        str = "ERROR"
+    }
 
-};
-
+    return str;
+}
+    
+repeatString("hey", 3)
+repeatString("hey", 10)
+repeatString("hey", 1)
+repeatString("hey", 0)
+repeatString("hey", -1)
 // Do not edit below this line
 module.exports = repeatString;
