1faf1b3756009843b87af7c7b876719b7093d918
Nicolas Vivas <nicolasvivas93@gmail.com>
2022-07-26 13:46:14 -0300

    Modify function according to specs

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const array = str.split('')
+    array.reverse()
+    let textReversed = array.join('')
+    return textReversed;
 };
+
+reverseString("hello")
+reverseString("hello there")
+reverseString("123! abc!")
+reverseString("")
 
 // Do not edit below this line
 module.exports = reverseString;
