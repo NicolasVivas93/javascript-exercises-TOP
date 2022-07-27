8ebef54727d51d56ebed5cd9426196b789248cf6
Nicolas Vivas <nicolasvivas93@gmail.com>
2022-07-27 17:03:30 -0300

    Resolve first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,elem) {
+    isInArray(arr,elem)
+    if (isInArray(arr,elem)) {
+        const nvoArray = arr.filter((values) => values !== elem);
+        return nvoArray;
+    } 
+    else {
+        return "Element already removed";
+    }
 };
+
+function isInArray(arr,elem) {
+    let validator = arr.indexOf(elem);
+    if (validator !== -1) { 
+        return true;
+    } else {
+        return false;
+    }
+}
+
+removeFromArray([1, 2, 3, 4], 3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
