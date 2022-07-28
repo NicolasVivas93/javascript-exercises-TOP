b9de698e93ccae6f4b3c0d742c33320398bc4fcd
Nicolas Vivas <nicolasvivas93@gmail.com>
2022-07-28 16:20:00 -0300

    Resolve and complete function according to specs

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,24 +1,16 @@
-const removeFromArray = function(arr,elem) {
-    isInArray(arr,elem)
-    if (isInArray(arr,elem)) {
-        const nvoArray = arr.filter((values) => values !== elem);
+const removeFromArray = function(arr,elem1,elem2,elem3,elem4) {
+    
+        const nvoArray = arr.filter((values) => values !== elem1 && values !== elem2 && values !== elem3 && values !== elem4);
         return nvoArray;
-    } 
-    else {
-        return "Element already removed";
-    }
-};
-
-function isInArray(arr,elem) {
-    let validator = arr.indexOf(elem);
-    if (validator !== -1) { 
-        return true;
-    } else {
-        return false;
-    }
-}
+    }; 
 
 removeFromArray([1, 2, 3, 4], 3)
+removeFromArray([1, 2, 3, 4], 3, 2)
+removeFromArray([1, 2, 3, 4], 7, "tacos")
+removeFromArray([1, 2, 3, 4], 7, 2)
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4)
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3)
+removeFromArray([1, 2, 3], "1", 3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
