604a94f5081a9b209da748f022fbe9166e41f30a
Nicolas Vivas <nicolasvivas93@gmail.com>
2022-08-01 16:51:57 -0300

    Added formula to functions and covert the results to Number according to spec

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,20 @@
-const ftoc = function() {
-
+const ftoc = function(farenheit) {
+  let result = ((farenheit - 32) * (5/9)).toFixed(1);
+  return Number(result);
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let result = ((celsius * (9/5)) + 32).toFixed(1); 
+  return Number(result);
 };
+
+ftoc(32)
+ftoc(100)
+ftoc(-100)
+
+ctof(0)
+ctof(73.2)
+ctof(-10)
 
 // Do not edit below this line
 module.exports = {
